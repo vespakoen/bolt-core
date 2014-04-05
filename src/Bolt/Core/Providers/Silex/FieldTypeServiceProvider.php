582f9dde8d7887bb3dea7ced52add7da767de47f
Koen Schmeets <k.schmeets@gmail.com>
2014-04-05 16:29:40 +0200

    Adding "dumb" types to get rid of validation errors

--- src/Bolt/Core/Providers/Silex/FieldTypeServiceProvider.php
+++ src/Bolt/Core/Providers/Silex/FieldTypeServiceProvider.php
@@ -27,9 +27,24 @@ class FieldTypeServiceProvider implements ServiceProviderInterface {
     protected function registerDefaultFieldTypes(Application $app)
     {
         $defaultFields = new FieldTypeCollection(array(
-            'text' => new TextFieldType,
+            'text' => new TextFieldType($app),
             'image' => FieldType::fromConfig('image'),
             'uploadcare' => FieldType::fromConfig('uploadcare'),
+            'slug' => FieldType::fromConfig('slug'),
+            'html' => FieldType::fromConfig('html'),
+            'video' => FieldType::fromConfig('video'),
+            'templateselect' => FieldType::fromConfig('templateselect'),
+            'geolocation' => FieldType::fromConfig('geolocation'),
+            'imagelist' => FieldType::fromConfig('imagelist'),
+            'file' => FieldType::fromConfig('file'),
+            'filelist' => FieldType::fromConfig('filelist'),
+            'checkbox' => FieldType::fromConfig('checkbox'),
+            'markdown' => FieldType::fromConfig('markdown'),
+            'datetime' => FieldType::fromConfig('datetime'),
+            'date' => FieldType::fromConfig('date'),
+            'integer' => FieldType::fromConfig('integer'),
+            'float' => FieldType::fromConfig('float'),
+            'select' => FieldType::fromConfig('select'),
             'textarea' => FieldType::fromConfig('textarea', array(
                 'column' => 'text',
                 'doctrine_type' => 'Doctrine\DBAL\Types\TextType'
