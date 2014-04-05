213b2ce8a3d7e77a4fa1320b464097fe558e86b9
Koen Schmeets <k.schmeets@gmail.com>
2014-04-05 03:36:33 +0200

    Updating ExtensionServiceProvider

--- src/Bolt/Core/Providers/Silex/ExtensionServiceProvider.php
+++ src/Bolt/Core/Providers/Silex/ExtensionServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Bolt\Core\Providers\Silex;
 
+use Bolt\Core\Extension\ExtensionCollection;
+
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -9,60 +11,13 @@ class ExtensionServiceProvider implements ServiceProviderInterface {
 
     public function register(Application $app)
     {
-        $this->registerExtensions($app);
+        $this->registerExtensionCollection($app);
     }
 
-    protected function registerDirectories(Application $app)
+    protected function registerExtensionCollection(Application $app)
     {
-        $app['config.directories'] = $app->share(function($app) {
-            $paths = array($app['paths.config']);
-
-            if(isset($app['env'])) {
-                $paths[] = $app['paths.config'].$app['env']);
-            }
-
-            return $paths;
-        });
-    }
-
-    protected function registerLocator(Application $app)
-    {
-        $app['config.locator'] = $app->share(function($app) {
-            return new ConfigLocator($app['config.directories']);
-        });
-    }
-
-    protected function registerContentTypeLoaders(Application $app)
-    {
-        $app['config.loaders'] = $app->share(function($app) {
-            return array(
-                new YamlContentTypeLoader($app['config.locator']),
-                new YamlFieldTypeLoader($app['config.locator']),
-            );
-        });
-    }
-
-    protected function registerResolver(Application $app)
-    {
-        $app['config.resolver'] = $app->share(function($app) {
-            return new ConfigLoaderResolver(
-                $app['config.locator'],
-                $app['config.loaders']
-            );
-        });
-    }
-
-    protected function registerLoader(Application $app)
-    {
-        $app['config.loader'] = $app->share(function($app) {
-            return new DelegatingLoader($app['config.resolver']);
-        });
-    }
-
-    protected function registerConfig(Application $app)
-    {
-        $app['config'] = $app->share(function($app) {
-            return new Config($app, $app['config.loader']);
+        $app['extensions'] = $app->share(function($app) {
+            return new ExtensionCollection;
         });
     }
 
