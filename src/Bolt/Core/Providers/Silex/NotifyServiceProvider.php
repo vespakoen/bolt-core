7b8f061d28c2581267902a9468669a3407b86711
Koen Schmeets <k.schmeets@gmail.com>
2014-04-05 03:35:40 +0200

    Adding NotifyServiceProvider

---
+++ src/Bolt/Core/Providers/Silex/NotifyServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bolt\Core\Providers\Silex;
+
+use Bolt\Core\Support\Notify;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class NotifyServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app)
+    {
+        $app['notify'] = $app->share(function($app) {
+            return new Notify($app);
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+
+}
