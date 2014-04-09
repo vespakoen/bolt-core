7caef7cc884f8dfa1b7aaeefdb7804d09bb487c8
Koen Schmeets <k.schmeets@gmail.com>
2014-04-09 23:36:17 +0200

    Adding serviceprovider for View

---
+++ src/Bolt/Core/Providers/Silex/ViewServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bolt\Core\Providers\Silex;
+
+use Bolt\Core\View\Factory\View;
+use Bolt\Core\View\ViewLoader;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class ViewServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app)
+    {
+        $this->registerViewFactories($app);
+    }
+
+    protected function registerViewFactories(Application $app)
+    {
+        $app['view.factory'] = $app->share(function($app) {
+            return new View($app);
+        });
+
+        $app['twig.loader'] = $app->share(function($app) {
+            return new ViewLoader($app['twig.path']);
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+
+}
