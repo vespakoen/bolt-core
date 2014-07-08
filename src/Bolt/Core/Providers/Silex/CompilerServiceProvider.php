45788df87e30dc6059e71e91088dbf1c34325d87
Koen Schmeets <k.schmeets@gmail.com>
2014-07-08 16:29:54 +0200

    Moving elasticsearch compiler registration to extension

--- src/Bolt/Core/Providers/Silex/CompilerServiceProvider.php
+++ src/Bolt/Core/Providers/Silex/CompilerServiceProvider.php
@@ -4,7 +4,6 @@ namespace Bolt\Core\Providers\Silex;
 
 use Bolt\Core\Compiler\CodyLaravelCompiler;
 use Bolt\Core\Compiler\CodyDoctrineCompiler;
-use Bolt\Core\Compiler\CodyElasticsearchCompiler;
 use Bolt\Core\Compiler\DoctrineYamlCompiler;
 
 use Silex\Application;
@@ -20,10 +19,6 @@ class CompilerServiceProvider implements ServiceProviderInterface
 
         $app['compiler.cody.doctrine'] = $app->share(function ($app) {
             return new CodyDoctrineCompiler($app);
-        });
-
-        $app['compiler.cody.elasticsearch'] = $app->share(function ($app) {
-            return new CodyElasticsearchCompiler($app);
         });
 
         $app['compiler.doctrine.yaml'] = $app->share(function ($app) {
