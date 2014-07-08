e723caa6ecd4c2916a2f2331758e9ccfcc445473
Koen Schmeets <k.schmeets@gmail.com>
2014-07-08 15:38:24 +0200

    Adding (initial stage of) generic setter/getter support

--- src/Bolt/Core/Content/Eloquent/Model.php
+++ src/Bolt/Core/Content/Eloquent/Model.php
@@ -2,7 +2,10 @@
 
 namespace Bolt\Core\Content\Eloquent;
 
+use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Eloquent\Model as Eloquent;
+
+use Bolt\Core\App;
 
 class Model extends Eloquent
 {
@@ -47,14 +50,46 @@ class Model extends Eloquent
      */
     public function outgoing()
     {
-        return $this->hasMany('Trapps\Domain\Model\Eloquent\Relations', 'from_id')
+        $config = App::make('config');
+        $vendor = $config->get('app/package/vendor');
+        $name = $config->get('app/package/name');
+
+        return $this->hasMany("{$vendor}\\{$name}\Model\Eloquent\Relations", 'from_id')
             ->where('from_type', '=', $this->getTable());
     }
 
     public function incoming()
     {
-        return $this->hasMany('Trapps\Domain\Model\Eloquent\Relations', 'to_id')
+        $config = App::make('config');
+        $vendor = $config->get('app/package/vendor');
+        $name = $config->get('app/package/name');
+
+        return $this->hasMany("{$vendor}\\{$name}\Model\Eloquent\Relations", 'to_id')
             ->where('to_type', '=', $this->getTable());
+    }
+
+    public function callBoltSetter($key, $value, $setter)
+    {
+        switch ($setter) {
+            case 'geojson_to_postgis':
+                $this->attributes[$key] = new Expression("ST_GeomFromGeoJSON('" . $value . "')");
+                break;
+
+            case 'to_json_if_array':
+                $this->attributes[$key] = is_array($value) || is_object($value) ? json_encode($value) : $value;
+                break;
+        }
+    }
+
+    public function callBoltGetter($value, $getter)
+    {
+        switch($getter) {
+            case 'from_json_if_json':
+                return substr($value, 0, 1) == "{" || substr($value, 0, 1) == "[" ? json_decode($value) : $value;
+                break;
+        }
+
+        return $value;
     }
 
 }
