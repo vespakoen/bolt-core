d101847f6e4dc4b7acbc56eeb6f7c7bc961bc736
Koen Schmeets <k.schmeets@gmail.com>
2014-07-30 02:17:06 +0200

    Adding initial redactor route

--- src/Bolt/Core/Controller/Async.php
+++ src/Bolt/Core/Controller/Async.php
@@ -31,11 +31,32 @@ class Async extends Controller implements ControllerProviderInterface
             ->bind('async.content')
             ->value('id', null);
 
+        $controllers->get('images', 'controller.async:getImages');
+
         $controllers->get('partial/{contentTypeKey}/form', 'controller.async:getForm')->bind('form');
 
         $controllers->get('migrate/', 'controller.async:getMigrate');
 
+
         return $controllers;
+    }
+
+    public function getImages(Request $request, Application $app)
+    {
+        return '[
+    { "thumb": "json/images/1_m.jpg", "image": "json/images/1.jpg", "title": "Image 1", "folder": "Folder 1" },
+    { "thumb": "json/images/2_m.jpg", "image": "json/images/2.jpg", "title": "Image 2", "folder": "Folder 1" },
+    { "thumb": "json/images/3_m.jpg", "image": "json/images/3.jpg", "title": "Image 3", "folder": "Folder 1" },
+    { "thumb": "json/images/4_m.jpg", "image": "json/images/4.jpg", "title": "Image 4", "folder": "Folder 1" },
+    { "thumb": "json/images/5_m.jpg", "image": "json/images/5.jpg", "title": "Image 5", "folder": "Folder 1" },
+    { "thumb": "json/images/1_m.jpg", "image": "json/images/1.jpg", "title": "Image 6", "folder": "Folder 1" },
+    { "thumb": "json/images/2_m.jpg", "image": "json/images/2.jpg", "title": "Image 7", "folder": "Folder 1" },
+    { "thumb": "json/images/3_m.jpg", "image": "json/images/3.jpg", "title": "Image 8", "folder": "Folder 1" },
+    { "thumb": "json/images/4_m.jpg", "image": "json/images/4.jpg", "title": "Image 9", "folder": "Folder 1" },
+    { "thumb": "json/images/5_m.jpg", "image": "json/images/5.jpg", "title": "Image 10", "folder": "Folder 2" },
+    { "thumb": "json/images/1_m.jpg", "image": "json/images/1.jpg", "title": "Image 11", "folder": "Folder 2" },
+    { "thumb": "json/images/2_m.jpg", "image": "json/images/2.jpg", "title": "Image 12", "folder": "Folder 2" }
+]';
     }
 
     public function getContent(Request $request, Application $app, $contentTypeKey, $id = null)
